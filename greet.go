8c5d9e28012637752f306881beb1fac9b1bb3077
Hrutvik Yadav <hrutvikyadav@gmail.com>
2023-09-11 22:51:12 +0530

    feat: return errors so caller can check for them and act

--- greet.go
+++ greet.go
@@ -1,13 +1,24 @@
 package go_greet // notice the difference this is a so called shared library
 // executable go programs have a main package instead of a named package
 
-import "fmt"
+import (
+    "fmt"
+    "errors"
 
-func Greet(name string) string {
+)
+
+
+func Greet(name string) ( string, error ) {
     // Function with Upcase 1st letter is callable from function in another package
     // Also called as exported name
+
+    // return error with message
+    if name == "" {
+        return "", errors.New("Greet Error => empty name")
+    }
+
     message := fmt.Sprintf("From %v motherfucker", name)
-    return message
+    return message, nil
     
 }
 
